a2f80725b80ec4c89ecbf351ba16af7117e46199
navin <navin1343@iiitd.ac.in>
2015-10-13 17:30:10 +0530

    userrole

---
+++ app/src/main/java/com/google/samples/quickstart/signin/MainActivity.java
@@ -0,0 +1,488 @@
+/*
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.samples.quickstart.signin;
+
+import android.accounts.Account;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.GoogleAuthException;
+import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.auth.UserRecoverableAuthException;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+//import com.google.android.gms.plus.Account;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+
+
+/**
+ * Minimal activity demonstrating basic Google Sign-In.
+ */
+public class MainActivity extends AppCompatActivity implements
+        View.OnClickListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private static final String TAG = "MainActivity";
+
+    /* RequestCode for resolutions involving sign-in */
+    private static final int RC_SIGN_IN = 9001;
+
+    /* Keys for persisting instance variables in savedInstanceState */
+    private static final String KEY_IS_RESOLVING = "is_resolving";
+    private static final String KEY_SHOULD_RESOLVE = "should_resolve";
+
+    /* Client for accessing Google APIs */
+    private GoogleApiClient mGoogleApiClient;
+
+    /* View to display current status (signed-in, signed-out, disconnected, etc) */
+    private TextView mStatus;
+
+    // [START resolution_variables]
+    /* Is there a ConnectionResult resolution in progress? */
+    private boolean mIsResolving = false;
+
+    /* Should we automatically resolve ConnectionResults when possible? */
+    private boolean mShouldResolve = false;
+    // [END resolution_variables]
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Restore from saved instance state
+        // [START restore_saved_instance_state]
+        if (savedInstanceState != null) {
+            mIsResolving = savedInstanceState.getBoolean(KEY_IS_RESOLVING);
+            mShouldResolve = savedInstanceState.getBoolean(KEY_SHOULD_RESOLVE);
+        }
+        // [END restore_saved_instance_state]
+
+        // Set up button click listeners
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
+        findViewById(R.id.disconnect_button).setOnClickListener(this);
+
+        // Large sign-in
+        ((SignInButton) findViewById(R.id.sign_in_button)).setSize(SignInButton.SIZE_WIDE);
+
+        // Start with sign-in button disabled until sign-in either succeeds or fails
+        findViewById(R.id.sign_in_button).setEnabled(false);
+
+        // Set up view instances
+        mStatus = (TextView) findViewById(R.id.status);
+
+        // [START create_google_api_client]
+        // Build GoogleApiClient with access to basic profile
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(new Scope(Scopes.PROFILE))
+                .addScope(new Scope(Scopes.PLUS_LOGIN))
+                .build();
+        // [END create_google_api_client]
+    }
+
+    private void updateUI(boolean isSignedIn) {
+
+
+        if (isSignedIn) {
+            // Show signed-in user's name
+           // Plus.PeopleApi.loadVisible(mGoogleApiClient, null).setResultCallback(this);
+            Person currentPerson = Plus.PeopleApi.getCurrentPerson(mGoogleApiClient);
+            if (currentPerson != null) {
+                String name = currentPerson.getDisplayName();
+                mStatus.setText(getString(R.string.signed_in_fmt, name));
+            } else {
+                Log.w(TAG, getString(R.string.error_null_person));
+                mStatus.setText(getString(R.string.signed_in_err));
+            }
+
+            // Set button visibility
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+        } else {
+            // Show signed-out message
+            mStatus.setText(R.string.signed_out);
+
+            // Set button visibility
+            findViewById(R.id.sign_in_button).setEnabled(true);
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
+        }
+    }
+
+    private void showSignedInUI() {
+        updateUI(true);
+    }
+
+    private void showSignedOutUI() {
+        updateUI(false);
+    }
+
+    // [START on_start_on_stop]
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+    // [END on_start_on_stop]
+
+    // [START on_save_instance_state]
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(KEY_IS_RESOLVING, mIsResolving);
+        outState.putBoolean(KEY_SHOULD_RESOLVE, mShouldResolve);
+    }
+    // [END on_save_instance_state]
+
+    // [START on_activity_result]
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult:" + requestCode + ":" + resultCode + ":" + data);
+
+        if (requestCode == RC_SIGN_IN) {
+            // If the error resolution was not successful we should not resolve further.
+            if (resultCode != RESULT_OK) {
+                mShouldResolve = false;
+            }
+
+            mIsResolving = false;
+            mGoogleApiClient.connect();
+        }
+    }
+    // [END on_activity_result]
+
+    // [START on_connected]
+    @Override
+    public void onConnected(Bundle bundle) {
+        // onConnected indicates that an account was selected on the device, that the selected
+        // account has granted any requested permissions to our app and that we were able to
+        // establish a service connection to Google Play services.
+        Log.d(TAG, "onConnected:" + bundle);
+        mShouldResolve = false;
+        new GetIdTokenTask().execute();
+
+       // Log.d(TAG, "Tokenid:" + sAccessToken);
+
+
+        // Show the signed-in UI
+        showSignedInUI();
+    }
+    // [END on_connected]
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        // The connection to Google Play services was lost. The GoogleApiClient will automatically
+        // attempt to re-connect. Any UI elements that depend on connection to Google APIs should
+        // be hidden or disabled until onConnected is called again.
+        Log.w(TAG, "onConnectionSuspended:" + i);
+    }
+
+    // [START on_connection_failed]
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // Could not connect to Google Play Services.  The user needs to select an account,
+        // grant permissions or resolve an error in order to sign in. Refer to the javadoc for
+        // ConnectionResult to see possible error codes.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+
+        if (!mIsResolving && mShouldResolve) {
+            if (connectionResult.hasResolution()) {
+                try {
+                    connectionResult.startResolutionForResult(this, RC_SIGN_IN);
+                    mIsResolving = true;
+                } catch (IntentSender.SendIntentException e) {
+                    Log.e(TAG, "Could not resolve ConnectionResult.", e);
+                    mIsResolving = false;
+                    mGoogleApiClient.connect();
+                }
+            } else {
+                // Could not resolve the connection result, show the user an
+                // error dialog.
+                showErrorDialog(connectionResult);
+            }
+        } else {
+            // Show the signed-out UI
+            showSignedOutUI();
+        }
+    }
+    // [END on_connection_failed]
+
+    private void showErrorDialog(ConnectionResult connectionResult) {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, RC_SIGN_IN,
+                        new DialogInterface.OnCancelListener() {
+                            @Override
+                            public void onCancel(DialogInterface dialog) {
+                                mShouldResolve = false;
+                                showSignedOutUI();
+                            }
+                        }).show();
+            } else {
+                Log.w(TAG, "Google Play Services Error:" + connectionResult);
+                String errorString = apiAvailability.getErrorString(resultCode);
+                Toast.makeText(this, errorString, Toast.LENGTH_SHORT).show();
+
+                mShouldResolve = false;
+                showSignedOutUI();
+            }
+        }
+    }
+
+    // [START on_click]
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                onSignInClicked();
+                break;
+            case R.id.sign_out_button:
+                onSignOutClicked();
+                break;
+            case R.id.disconnect_button:
+                onDisconnectClicked();
+                break;
+        }
+    }
+    // [END on_click]
+
+    // [START on_sign_in_clicked]
+    private void onSignInClicked() {
+        // User clicked the sign-in button, so begin the sign-in process and automatically
+        // attempt to resolve any errors that occur.
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            mShouldResolve = true;
+            mGoogleApiClient.connect();
+
+            // Show a message to the user that we are signing in.
+            mStatus.setText(R.string.signing_in);
+
+        } else {
+            Context context = getApplicationContext();
+            CharSequence text = "Please check your Network Connection";
+            int duration = Toast.LENGTH_LONG;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            return;
+        }
+
+    }
+    // [END on_sign_in_clicked]
+
+    // [START on_sign_out_clicked]
+    private void onSignOutClicked() {
+        // Clear the default account so that GoogleApiClient will not automatically
+        // connect in the future.
+        if (mGoogleApiClient.isConnected()) {
+            Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
+            mGoogleApiClient.disconnect();
+        }
+
+        showSignedOutUI();
+    }
+    // [END on_sign_out_clicked]
+
+    // [START on_disconnect_clicked]
+    private void onDisconnectClicked() {
+        // Revoke all granted permissions and clear the default account.  The user will have
+        // to pass the consent screen to sign in again.
+        if (mGoogleApiClient.isConnected()) {
+            Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
+            Plus.AccountApi.revokeAccessAndDisconnect(mGoogleApiClient);
+            mGoogleApiClient.disconnect();
+        }
+
+        showSignedOutUI();
+    }
+    // [END on_disconnect_clicked]
+
+    private class GetIdTokenTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String sAccessToken=null;
+            String accountName = Plus.AccountApi.getAccountName(mGoogleApiClient);
+            Account account = new Account(accountName, GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE);
+            //Account account = new Account(accountName, GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE);
+            //String scope = "oauth2:" + Scopes.PROFILE;
+           String scope =  "audience:server:client_id:" + "372355961536-g2pftp7d2pk50125c8vj6caiibghtrmr.apps.googleusercontent.com";
+
+           // String scope="oauth2:server:client_id:22114338477-c8mda46eg1mekcgmash0irp6s0v06j64.apps.googleusercontent.com:api_scope:https://www.googleapis.com/auth/plus.login";
+            //String scopes = "audience:server:client_id:" + SERVER_CLIENT_ID; // Not the app's client ID.
+            try {
+                 sAccessToken= GoogleAuthUtil.getToken(getApplicationContext(),account , scope);
+
+
+            } catch (IOException e) {
+                Log.e(TAG, "Error retrieving ID token.", e);
+
+            } catch (GoogleAuthException e) {
+                Log.e(TAG, "Error retrieving ID token.", e);
+
+            }
+            return sAccessToken;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //Log.i(TAG, "ID token: " + result);
+            if (result != null) {
+                // Successfully retrieved ID Token
+                // ...
+                Log.d(TAG, "Tokenid:" + result);
+                new connectRestApi().execute(result);
+            } else {
+                // There was some error getting the ID Token
+                // ...
+            }
+        }
+
+    }
+    private class connectRestApi extends AsyncTask <String, Void, String> {
+
+        protected String doInBackground(String... tokenid) {
+
+            try {
+                return downloadUrl("http://192.168.48.143:8080/cluborg.backend/rest/todo",tokenid[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+
+        }
+
+        protected void onPostExecute(String results) {
+            Context context = getApplicationContext();
+            //CharSequence text = "Hello toast!";
+            int duration = Toast.LENGTH_LONG;
+
+            Toast toast = Toast.makeText(context, results, duration);
+            toast.show();
+
+        }
+
+     }
+    private String downloadUrl(String myurl,String tokenid) throws IOException {
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Accept", "application/json");
+
+
+            JSONObject cred   = new JSONObject();
+            cred.put("tokenid", tokenid);
+
+            OutputStreamWriter wr= new OutputStreamWriter(conn.getOutputStream());
+            wr.write(cred.toString());
+            wr.flush();
+            //con.setRequestMethod("POST");
+            //conn.setRequestProperty("tokenid",tokenid);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d(TAG, "The response is: " + response);
+
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+    // Reads an InputStream and converts it to a String.
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+}
+
+
+
+
+
